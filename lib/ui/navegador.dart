4fd51bfde68cf2c54d27ebb989ae589fd13ab60f
FranckEP <90482775+FranckEP@users.noreply.github.com>
2021-11-29 11:29:42 -0500

    Update navegador.dart

--- lib/ui/navegador.dart
+++ lib/ui/navegador.dart
@@ -36,32 +36,35 @@ class _ContentPageState extends State<ContentPage>{
     return Scaffold(
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color(0xff3A434D),
+        
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.blur_circular_outlined, color: Colors.black,),
+            icon: Icon(Icons.blur_circular_outlined, color: Color(0xff68CAF0),),
             label: 'Estados',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.message_outlined, color: Colors.black,),
+            icon: Icon(Icons.message_outlined, color: Color(0xff68CAF0),),
             label: 'Mensajes',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline_outlined, color: Colors.black,),
+            icon: Icon(Icons.add_circle_outline_outlined, color: Color(0xff68CAF0),),
             label: 'Nuevo',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.photo_camera_back_outlined, color: Colors.black,),
+            icon: Icon(Icons.photo_camera_back_outlined, color: Color(0xff68CAF0),),
             label: 'Arte Social',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.menu, color: Colors.black,),
+            icon: Icon(Icons.menu, color: Color(0xff68CAF0),),
             label: 'Menú',
           ),
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
+        backgroundColor: const Color(0xff3A434D),
         selectedItemColor: const Color(0xff68CAF0),
+        unselectedItemColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
         ),
     );
   }
