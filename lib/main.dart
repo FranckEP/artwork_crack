6c2880571769617f5a67fcfd3d73b80e25f9ba17
FranckEP <90482775+FranckEP@users.noreply.github.com>
2021-11-30 12:23:21 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:artwork_crack/ui/myapp.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loggy/loggy.dart';
 
 void main() {
+  Loggy.initLoggy(
+    logPrinter: const PrettyPrinter(
+      showColors: true,
+    ),
+  );
+  Get.put({});
   runApp(const MyApp());
 }
 
